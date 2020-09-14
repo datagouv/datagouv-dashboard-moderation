8ded0cdface24e4748817ee69af30dde80345d18
Julien_P <jparis.py@gmail.com>
2020-09-14 16:36:14 +0200

    bug login moderation

--- frontend/src/components/moderation/ModerationCheckbox.vue
+++ frontend/src/components/moderation/ModerationCheckbox.vue
@@ -129,11 +129,11 @@ export default {
       this.emitResponse(respData)
       this.isLoading = false
     },
-    makeToast (updatedItem) {
+    makeToast (moderationResponse) {
       const h = this.$createElement
-      const variant = updatedItem.status !== 200 ? 'danger' : 'success'
-      const title = updatedItem.status !== 200 ? 'error' : 'success'
-      const msg = updatedItem.status !== 200 ? this.$t('toastsModeration.errorTxt', { code: updatedItem.status }) : 'ok msg'
+      const variant = moderationResponse.status !== 200 ? 'danger' : 'success'
+      const title = moderationResponse.status !== 200 ? 'error' : 'success'
+      const msg = moderationResponse.status !== 200 ? this.$t('toastsModeration.errorTxt', { code: moderationResponse.status }) : 'ok msg'
 
       const vNodesTitle = h(
         'div',
--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -112,7 +112,7 @@ export default {
       // log into moderation API
       const loginModerationResponse = await this.$MODERATIONcli.login(this.tokens.access.value)
       console.log('-V- Login > updateModeration > loginModerationResponse : ', loginModerationResponse)
-      await this.makeToast(loginModerationResponse)
+      // await this.makeToast(loginModerationResponse)
       this.$router.push(`/get-user-data?redirect=${this.redirection}`)
     } catch (ex) {
       await this.makeToast(ex)
@@ -126,38 +126,38 @@ export default {
       log: (state) => state.global.log,
       tokens: (state) => state.oauth.tokens
     })
-  },
-  methods: {
-    async makeToast (loginModerationResponse) {
-      console.log('-V- Login > makeToast > loginModerationResponse : ', loginModerationResponse)
-      const h = this.$createElement
-      const variant = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'success' : 'danger'
-      const title = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'success' : 'error'
-      const msg = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'ok msg' : this.$t('toastsModeration.errorTxt', { code: loginModerationResponse.status })
-
-      const vNodesTitle = h(
-        'div',
-        { class: ['d-flex', 'flex-grow-1', 'align-items-baseline', 'ml-2'] },
-        [
-          h('strong', { class: ['mr-2', 'text-center'] }, this.$t(`toastsModeration.${title}`))
-        ]
-      )
-      const vNodesMsg = h(
-        'p',
-        { class: ['text-center', 'my-2'] },
-        [
-          h('strong', msg)
-        ]
-      )
-      console.log('-V- Login > makeToast > vNodesTitle : ', vNodesTitle)
-
-      this.$bvToast.toast([vNodesMsg], {
-        title: [vNodesTitle],
-        variant: variant,
-        solid: true
-      })
-    }
   }
+  // methods: {
+  //   async makeToast (loginModerationResponse) {
+  //     console.log('-V- Login > makeToast > loginModerationResponse : ', loginModerationResponse)
+  //     const h = this.$createElement
+  //     const variant = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'success' : 'danger'
+  //     const title = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'success' : 'error'
+  //     const msg = loginModerationResponse.status && loginModerationResponse.status === 200 ? 'ok msg' : this.$t('toastsModeration.errorTxt', { code: loginModerationResponse.status })
+
+  //     const vNodesTitle = h(
+  //       'div',
+  //       { class: ['d-flex', 'flex-grow-1', 'align-items-baseline', 'ml-2'] },
+  //       [
+  //         h('strong', { class: ['mr-2', 'text-center'] }, this.$t(`toastsModeration.${title}`))
+  //       ]
+  //     )
+  //     const vNodesMsg = h(
+  //       'p',
+  //       { class: ['text-center', 'my-2'] },
+  //       [
+  //         h('strong', msg)
+  //       ]
+  //     )
+  //     console.log('-V- Login > makeToast > vNodesTitle : ', vNodesTitle)
+
+  //     this.$bvToast.toast([vNodesMsg], {
+  //       title: [vNodesTitle],
+  //       variant: variant,
+  //       solid: true
+  //     })
+  //   }
+  // }
 }
 
 </script>
