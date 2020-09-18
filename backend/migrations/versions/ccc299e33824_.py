d363a81a962c4046a5d24570e623cbf3a47f0bdf
Julien_P <jparis.py@gmail.com>
2020-09-18 11:54:58 +0200

    fixes backend migrations and enums dgftypes

--- backend/migrations/versions/ccc299e33824_.py
+++ backend/migrations/versions/ccc299e33824_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 01501605d02d
+Revision ID: ccc299e33824
 Revises: 
-Create Date: 2020-09-18 11:25:18.866849
+Create Date: 2020-09-14 12:36:37.435535
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '01501605d02d'
+revision = 'ccc299e33824'
 down_revision = None
 branch_labels = None
 depends_on = None
